0bcb8c02f9df84bd4fb13a078bcedccefb158dba
Gelareh Taban <gtaban@us.ibm.com>
2017-11-08 13:24:38 -0600

    Fix all the white spacing issues

--- Tests/HTTPTests/ServerTests.swift
+++ Tests/HTTPTests/ServerTests.swift
@@ -5,6 +5,7 @@
 //
 // See http://swift.org/LICENSE.txt for license information
 //
+
 import XCTest
 import Dispatch
 
@@ -19,7 +20,7 @@ class ServerTests: XCTestCase {
         XCTAssertNotNil(resolver.responseBody)
         XCTAssertEqual(HTTPResponseStatus.ok.code, resolver.response?.status.code ?? 0)
     }
-    
+
     func testEcho() {
         let testString = "This is a test"
         let request = HTTPRequest(method: .post, target: "/echo", httpVersion: HTTPVersion(major: 1, minor: 1), headers: ["X-foo": "bar"])
@@ -30,7 +31,7 @@ class ServerTests: XCTestCase {
         XCTAssertEqual(HTTPResponseStatus.ok.code, resolver.response?.status.code ?? 0)
         XCTAssertEqual(testString, resolver.responseBody?.withUnsafeBytes { String(bytes: $0, encoding: .utf8) } ?? "Nil")
     }
-    
+
     func testHello() {
         let request = HTTPRequest(method: .get, target: "/helloworld", httpVersion: HTTPVersion(major: 1, minor: 1), headers: ["X-foo": "bar"])
         let resolver = TestResponseResolver(request: request, requestBody: Data())
@@ -40,7 +41,7 @@ class ServerTests: XCTestCase {
         XCTAssertEqual(HTTPResponseStatus.ok.code, resolver.response?.status.code ?? 0)
         XCTAssertEqual("Hello, World!", resolver.responseBody?.withUnsafeBytes { String(bytes: $0, encoding: .utf8) } ?? "Nil")
     }
-    
+
     func testSimpleHello() {
         let request = HTTPRequest(method: .get, target: "/helloworld", httpVersion: HTTPVersion(major: 1, minor: 1), headers: ["X-foo": "bar"])
         let resolver = TestResponseResolver(request: request, requestBody: Data())
@@ -57,10 +58,10 @@ class ServerTests: XCTestCase {
         XCTAssertEqual(HTTPResponseStatus.ok.code, resolver.response?.status.code ?? 0)
         XCTAssertEqual("Hello, World!", resolver.responseBody?.withUnsafeBytes { String(bytes: $0, encoding: .utf8) } ?? "Nil")
     }
-    
+
     func testOkEndToEnd() {
         let receivedExpectation = self.expectation(description: "Received web response \(#function)")
-        
+
         let server = HTTPServer()
         do {
             try server.start(port: 0, handler: OkHandler().handle)
@@ -86,10 +87,10 @@ class ServerTests: XCTestCase {
             XCTFail("Error listening on port \(0): \(error). Use server.failed(callback:) to handle")
         }
     }
-    
+
     func testHelloEndToEnd() {
         let receivedExpectation = self.expectation(description: "Received web response \(#function)")
-        
+
         let server = HTTPServer()
         do {
             try server.start(port: 0, handler: HelloWorldHandler().handle)
@@ -116,7 +117,7 @@ class ServerTests: XCTestCase {
             XCTFail("Error listening on port \(0): \(error). Use server.failed(callback:) to handle")
         }
     }
-    
+
     func testSimpleHelloEndToEnd() {
         let receivedExpectation = self.expectation(description: "Received web response \(#function)")
         let simpleHelloWebApp = SimpleResponseCreator { (_, body) -> SimpleResponseCreator.Response in
@@ -126,14 +127,14 @@ class ServerTests: XCTestCase {
                 body: "Hello, World!".data(using: .utf8)!
             )
         }
-        
+
         let server = HTTPServer()
         do {
             try server.start(port: 0, handler: simpleHelloWebApp.handle)
         } catch {
             XCTFail("Error listening on port \(0): \(error). Use server.failed(callback:) to handle")
         }
-        
+
         let session = URLSession(configuration: .default)
         let url = URL(string: "http://localhost:\(server.port)/helloworld")!
         print("Test \(#function) on port \(server.port)")
@@ -158,11 +159,11 @@ class ServerTests: XCTestCase {
         server.stop()
         print("\(#function) stopping server")
     }
-    
+
     func testRequestEchoEndToEnd() {
         let receivedExpectation = self.expectation(description: "Received web response \(#function)")
         let testString="This is a test"
-        
+
         let server = HTTPServer()
         do {
             try server.start(port: 0, handler: EchoHandler().handle)
@@ -173,7 +174,7 @@ class ServerTests: XCTestCase {
             request.httpMethod = "POST"
             request.httpBody = testString.data(using: .utf8)
             request.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-            
+
             let dataTask = session.dataTask(with: request) { (responseBody, rawResponse, error) in
                 let response = rawResponse as? HTTPURLResponse
                 XCTAssertNil(error, "\(error!.localizedDescription)")
@@ -194,7 +195,7 @@ class ServerTests: XCTestCase {
             XCTFail("Error listening on port \(0): \(error). Use server.failed(callback:) to handle")
         }
     }
-    
+
     func testRequestKeepAliveEchoEndToEnd() {
         let receivedExpectation1 = self.expectation(description: "Received web response 1: \(#function)")
         let receivedExpectation2 = self.expectation(description: "Received web response 2: \(#function)")
@@ -202,7 +203,7 @@ class ServerTests: XCTestCase {
         let testString1="This is a test"
         let testString2="This is a test, too"
         let testString3="This is also a test"
-        
+
         let server = HTTPServer()
         do {
             try server.start(port: 0, handler: EchoHandler().handle)
@@ -213,7 +214,7 @@ class ServerTests: XCTestCase {
             request1.httpMethod = "POST"
             request1.httpBody = testString1.data(using: .utf8)
             request1.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-            
+
             let dataTask1 = session.dataTask(with: request1) { (responseBody, rawResponse, error) in
                 let response = rawResponse as? HTTPURLResponse
                 XCTAssertNil(error, "\(error!.localizedDescription)")
@@ -264,7 +265,7 @@ class ServerTests: XCTestCase {
                 receivedExpectation1.fulfill()
             }
             dataTask1.resume()
-            
+
             self.waitForExpectations(timeout: 10) { (error) in
                 if let error = error {
                     XCTFail("\(error)")
@@ -294,7 +295,7 @@ class ServerTests: XCTestCase {
             request1.httpMethod = "POST"
             request1.httpBody = testString1.data(using: .utf8)
             request1.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-            
+
             let dataTask1 = session.dataTask(with: request1) { (responseBody, rawResponse, error) in
                 let response = rawResponse as? HTTPURLResponse
                 XCTAssertNil(error, "\(error!.localizedDescription)")
@@ -366,25 +367,25 @@ class ServerTests: XCTestCase {
             XCTFail("Error listening on port \(0): \(error). Use server.failed(callback:) to handle")
         }
     }
-    
-    
+
+
     func testRequestLargeEchoEndToEnd() {
         let receivedExpectation = self.expectation(description: "Received web response \(#function)")
-        
+
         //Use a small chunk size to make sure that we're testing multiple HTTPBodyHandler calls
         let chunkSize = 1024
-        
+
         // Get a file we know exists
         let executableURL = URL(fileURLWithPath: CommandLine.arguments[0])
         let testExecutableData: Data
-        
+
         do {
             testExecutableData = try Data(contentsOf: executableURL)
         } catch {
             XCTFail("Could not create Data from contents of \(executableURL)")
             return
         }
-        
+
         var testDataLong = testExecutableData + testExecutableData + testExecutableData + testExecutableData
         let length = testDataLong.count
         let keep = 16385
@@ -392,9 +393,9 @@ class ServerTests: XCTestCase {
         if remove > 0 {
             testDataLong.removeLast(remove)
         }
-        
+
         let testData = Data(testDataLong)
-        
+
         let server = PoCSocketSimpleServer()
         do {
             try server.start(port: 0, maxReadLength: chunkSize, handler: EchoHandler().handle)
@@ -446,7 +447,7 @@ class ServerTests: XCTestCase {
         XCTAssertNotNil(testExecutableData)
         
         let executableLength = testExecutableData.count
-        
+                
         let server = PoCSocketSimpleServer()
         do {
             let testHandler = AbortAndSendHelloHandler()
@@ -479,13 +480,13 @@ class ServerTests: XCTestCase {
             XCTFail("Error listening on port \(0): \(error). Use server.failed(callback:) to handle")
         }
     }
-    
-    
+
+
     func testExplicitCloseConnections() {
         let expectation = self.expectation(description: "0 Open Connection")
         let server = PoCSocketSimpleServer()
         let keepAliveTimeout = 0.1
-        
+
         do {
             try server.start(port: 0, keepAliveTimeout: keepAliveTimeout, handler: OkHandler().handle)
             
@@ -500,7 +501,7 @@ class ServerTests: XCTestCase {
                 #if os(Linux)
                     XCTAssertEqual(server.connectionCount, 0)
                     expectation.fulfill()
-                    
+                
                     // Darwin's URLSession replaces the `Connection: close` header with `Connection: keep-alive`, so allow it to expire
                 #else
                     DispatchQueue.main.asyncAfter(deadline: .now() + keepAliveTimeout) {
@@ -521,7 +522,7 @@ class ServerTests: XCTestCase {
             XCTFail("Error listening on port \(0): \(error). Use server.failed(callback:) to handle")
         }
     }
-    
+
     static var allTests = [
         ("testEcho", testEcho),
         ("testHello", testHello),
@@ -535,5 +536,5 @@ class ServerTests: XCTestCase {
         ("testRequestLargeEchoEndToEnd", testRequestLargeEchoEndToEnd),
         ("testExplicitCloseConnections", testExplicitCloseConnections),
         ("testRequestLargePostHelloWorld", testRequestLargePostHelloWorld),
-        ]
+    ]
 }
